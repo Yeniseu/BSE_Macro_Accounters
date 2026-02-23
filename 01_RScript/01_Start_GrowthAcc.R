4a389956aa394462cc3d8a94f9f8c9bda080bcc4
Orhun <orhun1337@gmail.com>
2026-02-24 00:05:28 +0100

    Start with Japan

---
+++ 01_RScript/01_Start_GrowthAcc.R
@@ -0,0 +1,47 @@
+# Authors: Ellie Walters, Ece Taşan Özel, Orhun Özel, Lea Röller
+# Date   : 23/02/2026
+# Scope  : Growth Accounting
+rm(list = ls())
+library(data.table)
+library(readxl)
+options(print.max = 300, scipen = 50, digits = 3)
+
+#### a
+### Load & prepare
+# Select Japan
+inp_path    <- "02_Input/JP_national_accounts.xlsx"
+sheet_names <- c("VA_Q", "VA_CP", "EMP", "COMP", "H_EMP", "H_EMPE")
+
+dt <- data.table(NULL)
+for (i in sheet_names) { # Read every sheet, rowbind them
+  tmp <- read_xlsx(inp_path, sheet = i) |> as.data.table()
+  dt <- rbind(dt, tmp)
+}
+
+# Melt the data table with relevant columns
+NACE_codes <- unique(dt[, .(geo_name, nace_r2_code, nace_r2_name)])
+dt[, c("geo_code", "geo_name", "nace_r2_name") := NULL]
+dt <- melt(dt, id.vars = c("nace_r2_code", "var"))
+dt <- dcast(dt, "nace_r2_code + variable ~ var")
+setnames(dt, c("nace_r2_code", "variable"), c("nace", "year"))
+setkey(dt, "nace", "year")
+
+# Select years
+dt[, year := as.numeric(as.character(year))]
+dt <- dt[between(year,1995, 2015)]
+
+#### b
+### Aggregate Output per Worker
+dt[, y_pw  := VA_Q / EMP]
+dt[, y_phw := VA_Q / H_EMP]
+plot(dt[nace == "TOT", year], dt[nace == "TOT", y_pw], type = "l", lwd = 2,
+     main = paste0("Japan - Output per Efficient Worker"),
+     xlab = "", ylab = "")
+plot(dt[nace == "TOT", year], dt[nace == "TOT", y_phw], type = "l", lwd = 2,
+     main = paste0("Japan - Output per Efficient Worker"),
+     xlab = "", ylab = "")
+
+#### c
+
+
+
