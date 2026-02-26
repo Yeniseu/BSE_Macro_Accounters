4c8e5b0c98f37c977397a5af5d4e7323cf21f37d
Orhun <orhun1337@gmail.com>
2026-02-26 19:07:38 +0100

    same

---
+++ 01_RScript/02_Answer_D_Japan.R
@@ -0,0 +1,15 @@
+# Authors: Ellie Walters, Ece Taşan Özel, Orhun Özel, Lea Röller
+# Date   : 23/02/2026
+# Scope  : Growth Accounting Q1 D,E
+
+rm(list = ls())
+library(data.table) # sorry for the inconvenience:)
+library(readxl)
+library(writexl)
+library(dplyr) # sorry, I am a dplyr girl;)
+options(print.max = 300, scipen = 50, digits = 2)
+
+
+#### Load data from previous script
+dt         <- readRDS("02_Input/dt_Japan.rds")
+growth_acc <- readRDS("02_Input/growth_acc_Japan.rds")
--- 01_RScript/04_Answer_FG_Japan.R
+++ 01_RScript/04_Answer_FG_Japan.R
@@ -1,6 +1,6 @@
 # Authors: Ellie Walters, Ece Taşan Özel, Orhun Özel, Lea Röller
 # Date   : 23/02/2026
-# Scope  : Growth Accounting Q1 D,E
+# Scope  : Growth Accounting Q1 F,G
 
 rm(list = ls())
 library(data.table) # sorry for the inconvenience:)
@@ -11,4 +11,5 @@ options(print.max = 300, scipen = 50, digits = 2)
 
 
 #### Load data from previous script
+dt         <- readRDS("02_Input/dt_Japan.rds")
 growth_acc <- readRDS("02_Input/growth_acc_Japan.rds")
