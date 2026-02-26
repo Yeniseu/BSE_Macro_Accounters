19b55c6394ca669d4323d0710cd51e69a48bb90f
Orhun <orhun1337@gmail.com>
2026-02-26 20:01:45 +0100

    D_ Create Sector

--- 01_RScript/02_Answer_D_Japan.R
+++ 01_RScript/02_Answer_D_Japan.R
@@ -18,19 +18,18 @@ NACE_codes <- readRDS("02_Input/NACE_codes.rds")
 #### Select broad NACE sectors
 dt[, unique(nace)]
 NACE_wanted <- c("A", "B", "C", "D-E", "F", "G", "H", "I", "J", "K", "L", "M-N", "O-U")
-# We don't have O-U, calculate it
+# We don't have O-U, calculate it by summing or averaging sectors from O to U
 dt_ou <- dt[nace %in% c("O-Q","RS","T", "U"), ]
 dt_ou[, nace := "O-U"]
-dt_ou[, sum(.SD), by = c("year", "nace")]
+summable   <- setdiff(names(dt_ou), c("nace", "year", "hc_pwt"))
+averageble <- c("hc_pwt")
+tmp1 <- dt_ou[, lapply(.SD, sum, na.rm=T), by = c("year", "nace"), .SDcols=summable]
+tmp2 <- dt_ou[, lapply(.SD, mean, na.rm=T), by = c("year", "nace"), .SDcols=averageble]
+dt_ou <- cbind(tmp1, tmp2[, ..averageble])
+# Merge O-U into dt and select the sectors wanted
+dt <- rbind(dt, dt_ou)
+dt <- dt[nace %in% NACE_wanted, ]
 
 
-new_nace <- NACE_codes[, .(nace=nace_r2_code, nace_new=nace_r2_code)]
-new_nace[grepl("^C", nace), nace_new:="C"]
-new_nace[nace%in% c("D", "E"), nace_new :="D-E"]
-new_nace[nace%in% c("O", "Q"), nace_new :="D-E"]
-
-
-O-Q
-R-S
-M-N 
-L68A
+#### Output per workers
+dt[]
