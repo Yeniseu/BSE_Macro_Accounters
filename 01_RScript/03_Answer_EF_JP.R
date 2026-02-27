931715d4ec61c655185638eb3d9364d463789975
Lea Kathrin Roeller <142701998+Leakathr1n@users.noreply.github.com>
2026-02-27 18:44:58 +0100

    Update 03_Answer_EF_JP.R

--- 01_RScript/03_Answer_EF_JP.R
+++ 01_RScript/03_Answer_EF_JP.R
@@ -7,6 +7,7 @@ library(data.table) # sorry for the inconvenience:)
 library(readxl)
 library(writexl)
 library(dplyr) # sorry, I am a dplyr girl;)
+library(kableExtra)
 options(print.max = 300, scipen = 50, digits = 2)
 
 
